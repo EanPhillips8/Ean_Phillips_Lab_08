76f5d21038569d19d8b34194d3ca3b5b0539f206
EanPhillips8 <eanphillips8@gmail.com>
2022-10-27 21:11:38 -0400

    uogi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double price = 0;
+        double total =+ price;
+        boolean done = false;
+        Scanner in = new Scanner(System.in);
+
+
+
+        {
+            String userName = SafeInput.getNonZerolength(in, "What is your username?");
+            System.out.println("Your username is: " + userName);
+        }
+        int favNum =SafeInput.getInt(in,"What is your favorite number");
+        System.out.println("Your favorite number is: " + favNum);
+        {
+            double favDouble = SafeInput.getDouble(in, "What is your favorite double");
+            System.out.println("Your favorite double is: " + favDouble);
+        }
+        {
+            int birthYear = SafeInput.getRangedInt(in, "What year were you born", 1950, 2010);
+            int birthMonth = SafeInput.getRangedInt(in, "What month were your born", 1, 12);
+            int birthDay = SafeInput.getRangedInt(in, "What day were you born", 1, 30);
+            int birthHour =SafeInput.getRangedInt(in,"What hour were you born",1,24);
+            int birthMinute =SafeInput.getRangedInt(in,"What minute were you born",1,59);
+            System.out.println("Your approximate date and time of birth is: " + birthMonth + "/" + birthDay+ "/" + birthYear+ " at " + birthHour+ ":" + birthMinute);
+        }
+        {
+            String ssn = SafeInput.getRegExString(in, "Enter your ss number", "\\d{3}-\\d{2}-\\d{4}");
+            System.out.println("Your ssn is " + ssn);
+        }
+        String mNum = SafeInput.getRegExString(in, "Enter your M number",  "(M|m)\\d{5}");
+        System.out.println("Your M number is: " + mNum);
+        {
+        String menu = SafeInput.getRegExString(in, "Choose one of the following options, Open-Save-View-Quit ", "[OoSsVvQq]");
+            System.out.println("Loading...");
+        }
+        {
+            double temp=SafeInput.CtoF(in,"What is the temp in Celsius");
+            System.out.println("Your temp in fahrenheit is " + temp);
+        }
     }
 }
--- src/dollarShop.java
+++ src/dollarShop.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class dollarShop {
+import java.util.Scanner;
+
+public class dollarShop {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean done = false;
+        double price=0;
+        double total=0;
+        {
+            do {
+                price = SafeInput.getRangedDouble(in, "Enter the price of your item", 0.50, 9.99);
+                total += price;
+                done = SafeInput.getYN(in, "Do you want to keep shopping ");
+                if (!done)
+                    System.out.println("Your total is: " + total);
+
+            } while (done);
+        }
+    }
 }
